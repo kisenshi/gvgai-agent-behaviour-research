fdc0857869fcc41ab2396b07b56e1693dbeb9b9b
kisenshi <kisenshi@users.noreply.github.com>
2021-02-16 12:05:04 +0000

    Fix refactor issue
    Update missing import

--- src/heuristic_diversification/mapelites/Performance.java
+++ src/heuristic_diversification/mapelites/Performance.java
@@ -9,7 +9,7 @@ import java.lang.reflect.Field;
 
 import org.apache.commons.math3.stat.descriptive.StatisticalSummaryValues;
 
-import heuristic_diversification.helper.GameStats;
+import heuristic_diversification.model.GameStats;
 
 public enum Performance {
     FAST("gameOverTick", true),
