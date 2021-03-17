9b0167df682bcfd3850ee78f9bd39555e2d2b54a
kisenshi <kisenshi@users.noreply.github.com>
2021-03-17 17:03:04 +0000

    Add CURIOUS as Behaviour

--- src/heuristic_diversification/config/Behaviours.java
+++ src/heuristic_diversification/config/Behaviours.java
@@ -11,7 +11,8 @@ import heuristic_diversification.framework.ArcadeMachineHeuristic;
 public enum Behaviours {
 
     WINNER(0, "WinningAndScoreHeuristic"), 
-    EXPLORER(1, "ExplorationHeuristic");
+    EXPLORER(1, "ExplorationHeuristic"),
+    CURIOUS(2, "CuriosityHeuristic");
 
     int id;
     String fileName;
