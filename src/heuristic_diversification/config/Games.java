6f59288679c694a6e50dfc15303f1ebac9d3e347
kisenshi <kisenshi@users.noreply.github.com>
2021-08-09 14:42:38 +0100

    Add available locations to each level
    Update Games config file

--- src/heuristic_diversification/config/Games.java
+++ src/heuristic_diversification/config/Games.java
@@ -7,10 +7,10 @@ package heuristic_diversification.config;
 
 public enum Games {
 
-    BUTTERFLIES(0, "butterflies", new int[]{ 206, 215, 217, 225, 235}),
-    ZELDA(1, "zelda", new int[]{ 64, 126, 130, 114, 138, 120, 127 }),
-    DIGDUG(2, "digdug", new int[]{ 405 }),
-    SHERIFF(3, "sheriff", new int[]{ 280 });
+    BUTTERFLIES(0, "butterflies", new int[]{ 206, 215, 217, 225, 235, 206}),
+    ZELDA(1, "zelda", new int[]{ 64, 126, 130, 114, 138, 120, 121 }),
+    DIGDUG(2, "digdug", new int[]{ 405, 405, 405, 405, 405, 405 }),
+    SHERIFF(3, "sheriff", new int[]{ 280, 280, 280, 280, 280, 280 });
 
     int id;
     String gameName;
