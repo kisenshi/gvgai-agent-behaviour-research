b9b9c5fbfe9957a65472e8403481f7f3838f3c4a
kisenshi <kisenshi@users.noreply.github.com>
2021-08-25 10:52:38 +0100

    Fix comment in testLevel

--- src/heuristic_diversification/testLevel.java
+++ src/heuristic_diversification/testLevel.java
@@ -25,7 +25,7 @@ public class testLevel {
 		
 		// Game and level to play
 		int gameIdx = 2;
-		int levelIdx = 5; // level names from 0 to 4 (game_lvlN.txt).
+		int levelIdx = 5; // level names from 0 to 5 (game_lvlN.txt).
 		String gameName = games[gameIdx][1];
 		String game = games[gameIdx][0];
 		String level = game.replace(gameName, gameName + "_lvl" + levelIdx);
